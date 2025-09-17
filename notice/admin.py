ea093b3cf1ad93e349cf4ed52f0c7140ab184537
Валерий <valev.@yandex.ru>
2025-09-17 05:38:59 +0500

    Добавлена send_news в admin.py. Реализовано создание и рассылка новостей через панель администратора.

--- notice/admin.py
+++ notice/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
-
-from notice.models import Ad, Category, Comment
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from MMORPG import settings
+from notice.models import Ad, Category, Comment, News
 
 admin.site.register(Ad)
 admin.site.register(Category)
 admin.site.register(Comment)
+
+
+def send_news(modeladmin, request, queryset):
+    for news_one in queryset:
+        subject = news_one.title
+        message = news_one.content
+        users_emails = [user.email for user in User.objects.all()]
+        print(users_emails)
+        from_email = settings.DEFAULT_FROM_EMAIL
+        recipient_list = users_emails  # Замените на список получателей
+        send_mail(subject, message, from_email, recipient_list)
+    redirect(request.META.get("HTTP_REFERER"))  # Перенаправление на список новостей
+
+@admin.register(News)
+class News(admin.ModelAdmin):
+    actions = [send_news]
+
+send_news.short_description = 'отправить рассылку по выбранной новости'
--- notice/models.py
+++ notice/models.py
@@ -9,6 +9,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
 class Ad(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -20,6 +24,10 @@ class Ad(models.Model):
     def get_absolute_url(self):
         return reverse_lazy('ad_detail', kwargs={'pk': self.pk})
 
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
 class Comment(models.Model):
     STATUS_CHOICES = (("pending","на рассмотрении"),("accept","принят"),("reject","отклонен"))
     ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
@@ -28,6 +36,26 @@ class Comment(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
 
+    def __str__(self):
+        return self.text[:25]
+    class Meta:
+        verbose_name = 'Отклик'
+        verbose_name_plural = 'Отклики'
+
 class UserCode(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     code = models.CharField(max_length=255, null=True, blank=True)
+
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
